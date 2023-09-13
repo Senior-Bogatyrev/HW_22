4c5b16a8910d89bd13480d0e8b34e36a9573d41a
Senior-Bogatyrev <jusuf94@mail.ru>
2023-09-14 00:31:25 +0300

    first commit

---
+++ HW_22.py
@@ -0,0 +1,45 @@
+class InfoMessage:
+    """Информационное сообщение о тренировке."""
+    pass
+
+
+class Training:
+    """Базовый класс тренировки."""
+
+
+
+class Running(Training):
+    """Тренировка: бег."""
+    pass
+
+
+class SportsWalking(Training):
+    """Тренировка: спортивная ходьба."""
+    pass
+
+
+class Swimming(Training):
+    """Тренировка: плавание."""
+    pass
+
+
+def read_package(workout_type: str, data: list) -> Training:
+    """Прочитать данные полученные от датчиков."""
+    pass
+
+
+def main(training: Training) -> None:
+    """Главная функция."""
+    pass
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
